6c0aef9447b9a5ea04bc391a61c11331c918dd46
Ananta <codeantu@gmail.com>
2025-11-18 21:41:37 +0530

    feat(database): Update database connection string in .env file

--- packages/db/prisma/migrations/20251118153033_init/migration.sql
+++ packages/db/prisma/migrations/20251118153033_init/migration.sql
@@ -1,33 +1,15 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "photo" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-  - You are about to drop the column `roomId` on the `Chat` table. All the data in the column will be lost.
-  - You are about to drop the `Room` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `RoomMember` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `spaceId` to the `Chat` table without a default value. This is not possible if the table is not empty.
-
-*/
--- DropForeignKey
-ALTER TABLE "Chat" DROP CONSTRAINT "Chat_roomId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Room" DROP CONSTRAINT "Room_adminId_fkey";
-
--- DropForeignKey
-ALTER TABLE "RoomMember" DROP CONSTRAINT "RoomMember_roomId_fkey";
-
--- DropForeignKey
-ALTER TABLE "RoomMember" DROP CONSTRAINT "RoomMember_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Chat" DROP COLUMN "roomId",
-ADD COLUMN     "spaceId" TEXT NOT NULL;
-
--- DropTable
-DROP TABLE "Room";
-
--- DropTable
-DROP TABLE "RoomMember";
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Space" (
@@ -38,6 +20,18 @@ CREATE TABLE "Space" (
     "adminId" TEXT NOT NULL,
 
     CONSTRAINT "Space_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" TEXT NOT NULL,
+    "spaceId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -52,6 +46,9 @@ CREATE TABLE "SpaceMember" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "SpaceMember_spaceId_userId_key" ON "SpaceMember"("spaceId", "userId");
 
 -- AddForeignKey
@@ -59,6 +56,9 @@ ALTER TABLE "Space" ADD CONSTRAINT "Space_adminId_fkey" FOREIGN KEY ("adminId") 
 
 -- AddForeignKey
 ALTER TABLE "Chat" ADD CONSTRAINT "Chat_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "SpaceMember" ADD CONSTRAINT "SpaceMember_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
